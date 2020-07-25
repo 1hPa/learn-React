1cb9f0a2cd248056bd34e503b9fe122b98e41128
Yu <tuba0927hy@gmail.com>
2020-07-26 00:16:26 +0900

    feat(classgame): add handleClick

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -56,4 +56,21 @@ class Game extends React.Component {
         xIsNext: true
     }
   };
+
+  handleClick(i){
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        squares: squares
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
 }
