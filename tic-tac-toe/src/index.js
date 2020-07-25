cc7957ae4124361d3aff6c8a78bf8277457253ed
Yu <tuba0927hy@gmail.com>
2020-07-26 00:03:34 +0900

    feat(classgame): add constructor

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -42,3 +42,18 @@ class Board extends React.Component {
     );
   }
 }
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       history: [
+        {
+          squares: Array(9).fill(null)
+        }
+       ],
+        stepNumber: 0,
+        xIsNext: true
+    }
+  };
+}
