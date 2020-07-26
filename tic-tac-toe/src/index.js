8061ba37dd08c1f5c986c7d5f358bb1bc5131a54
Yu <tuba0927hy@gmail.com>
2020-07-26 16:10:36 +0900

    feat(render): add return

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -104,4 +104,20 @@ class Game extends React.Component {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
   }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
 }
