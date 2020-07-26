c279e4cefb89ca97e3f835fe19bae41e60391f60
Yu <tuba0927hy@gmail.com>
2020-07-26 16:07:03 +0900

    feat: add render function

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -73,10 +73,35 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext
     });
   }
+
   jumpTo(step){
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2) === 0
     });
+  }
+
+  render(){
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key = {move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    }else{
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
   }
 }
