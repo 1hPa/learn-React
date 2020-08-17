94ebf1a7c8df18c80519a3efb108009ef1a48706
Yu <tuba0927hy@gmail.com>
2020-08-17 23:21:34 +0900

    fix: forget to put parentheses

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -67,7 +67,9 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       history: history.concat([
+        {
         squares: squares
+        }
       ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
